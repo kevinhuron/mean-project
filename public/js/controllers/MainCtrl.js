65bad607eb70f7a29f49fa6c1e2d528350024808
Kévin <kevin@cosavostra.com>
2016-06-03 18:49:19 +0200

    un peu de style

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,5 +2,7 @@
  * Created by kevinhuron on 25/05/2016.
  */
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
-    $scope.tagline = 'To the moon and back!';
+    $scope.image1 = 'img/img-1.jpg';
+    $scope.image2 = 'img/img-2.jpg';
+    $scope.image3 = 'img/img-3.jpg';
 });
