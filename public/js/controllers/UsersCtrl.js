a1817ddd45810cf28515a79073c26a678cbeee83
Kévin <kevin@cosavostra.com>
2016-06-03 15:49:56 +0200

    loader à la va vite pour l'inscription, jy retournerai plus tard, on le voi pas ça va trop vite Mdr

--- public/js/controllers/UsersCtrl.js
+++ public/js/controllers/UsersCtrl.js
@@ -19,18 +19,23 @@ angular.module('UsersCtrl', ['UsersService']).controller('UsersController', func
         $scope.passwordRequired = '';
         $scope.registrationFailed = '';
         $scope.registrationSuccess ='';
+        $scope.loader = true;
         if (!$scope.user.firstname || !$scope.user.lastname || !$scope.user.mail || !$scope.user.password) {
             if (!$scope.user.firstname) {
                 $scope.firstnameRequired = 'Votre Nom est requis';
+                $scope.loader = false;
             }
             if (!$scope.user.lastname) {
                 $scope.lastnameRequired = 'Votre prénom est requis';
+                $scope.loader = false;
             }
             if (!$scope.user.mail) {
                 $scope.mailRequired = 'Votre mail est requis et vous servira à vous connecter';
+                $scope.loader = false;
             }
             if (!$scope.user.password) {
                 $scope.passwordRequired = 'Un mot de passe est requis pour vous connecter';
+                $scope.loader = false;
             }
         } else {
             userData = {"firstname":$scope.user.firstname, "lastname":$scope.user.lastname, "mail":$scope.user.mail, "passwd":$scope.user.password};
@@ -38,8 +43,10 @@ angular.module('UsersCtrl', ['UsersService']).controller('UsersController', func
                 var registration = registr.data;
                 if (registration == "NOK") {
                     $scope.registrationFailed = 'Votre inscription à échoué ! Vérifié vos informations (Il se peut que votre email soit déjà dans notre base)';
+                    $scope.loader = false;
                 } else {
                     $scope.registrationSuccess = 'Votre inscription à bien été prise en compte !';
+                    $scope.loader = false;
                 }
             });
         }
